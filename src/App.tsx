0a8fb4bdb944c9024ea47cfdb86c882972e69f32
Marcosparral <m.parra.lattes@gmail.com>
2025-12-01 15:50:33 -0300

    feat: Se crea codigo css, el fomulario y se agregan las mnedas para cotizar criptos.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
+import { useEffect } from "react"
+import CriptoSearchForm from "./components/CriptoSearchForm"
+import { useCryptoStore } from "./store/store"
 
 
 function App() {
+  const fetchCryptos = useCryptoStore((state) => state.fetchCryptos)
 
+  useEffect(() => {
+    fetchCryptos()
+  }, [])
 
   return (
     <>
-     <div>Hola mundo</div>
+      <div className="container">
+        <h1 className="app-title">Cotizador de 
+          <span>Criptomonedas</span>
+        </h1>
+        <div className="content">
+          <CriptoSearchForm />
+        </div>
+      </div>
     </>
   )
 }
